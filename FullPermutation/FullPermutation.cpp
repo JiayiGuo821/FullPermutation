1dae2760484b3a4f95300eac4ad526bf699e213b
Jiayi Guo <guojy821@gmail.com>
2020-10-08 22:20:12 +0800

    1008v1

--- FullPermutation/FullPermutation.cpp
+++ FullPermutation/FullPermutation.cpp
@@ -1,8 +1,110 @@
 #include <iostream>
 #include <vector> 
+#include <deque>
 #include <string.h>
 
 using namespace std;
+
+int Permutation2Intermediary(int permutation, char method)
+{
+	deque<int> permutation_deq;
+	int intermediary = 0;
+
+	while (permutation)
+	{
+		permutation_deq.push_front(permutation % 10);
+		permutation = permutation / 10;
+	}
+
+	deque<int> intermediary_deq;
+	switch (method)
+	{
+	case '1':
+		{
+			for (int i = 0; i < permutation_deq.size(); i++)
+			{
+				int count = 0;
+				for (int j = i + 1; j < permutation_deq.size(); j++)
+				{
+					if (permutation_deq.at(j) < permutation_deq.at(i))
+						count++;
+				}
+				intermediary_deq.push_back(count);
+			}
+			break;
+		}
+	case '2':
+		{
+			break;
+		}
+	case '3':
+		{
+			break;
+		}
+	case '4':
+		{
+			break;
+		}
+	default:
+		break;
+	}
+	
+	for (int i = 0; i < intermediary_deq.size() - 1; i++)
+	{
+		intermediary = intermediary * 10;
+		intermediary += intermediary_deq.at(i);
+	}
+	
+	return intermediary;
+}
+
+
+int Intermediary2Rank(int intermediary, char method)
+{
+	deque<int> intermediary_deq;
+	int rank;
+
+	while (intermediary)
+	{
+		intermediary_deq.push_front(intermediary % 10);
+		intermediary = intermediary / 10;
+	}
+
+	deque<int> rank_deq;
+	switch (method)
+	{
+	case '1':
+		{
+			for (int i = 0; i < permutation_deq.size(); i++)
+			{
+				int count = 0;
+				for (int j = i + 1; j < permutation_deq.size(); j++)
+				{
+					if (permutation_deq.at(j) < permutation_deq.at(i))
+						count++;
+				}
+				intermediary_deq.push_back(count);
+			}
+			break;
+		}
+	case '2':
+		{
+			break;
+		}
+	case '3':
+		{
+			break;
+		}
+	case '4':
+		{
+			break;
+		}
+	default:
+		break;
+	}
+	return 0;
+}
+
 
 void main()
 {
@@ -14,8 +116,18 @@ void main()
 		cout << "无效输入，请重新输入：";
 		cin >> num;
 	}
+
+	char method;
+	printf("%s","键入全排列方法：1.字典序法 2.递增进位制法 3.递减进位制法 4.邻域对换法：");
+	cin >> method;
+	while (method < '1' || method > '4')
+	{
+		cout << "无效输入，请重新输入：";
+		cin >> method;
+	}
+
 	char option;
-	printf("%s","键入输入的内容：1.排列 2.序号数（1或2）：");
+	printf("%s","键入输入的内容：1.排列 2.序号数 3.中介数（1或2或3）：");
 	cin >> option; 
 	while (option != '1' && option != '2' && option != '3')
 	{
@@ -33,27 +145,90 @@ void main()
 	{
 	case '1':
 		{
-			int element;
-			vector<char> permutation;
-			for (int i = 0; i < int(num - '0'); i++)
+			int permutation;
+			cout << "输入一个排列:";
+			cin >> permutation;
+			
+			int rank, intermediary;
+			intermediary = Permutation2Intermediary(permutation, method);
+			cout << "对应的中介数为：" << intermediary << endl;
+			rank = Intermediary2Rank(intermediary, method);
+			cout << "对应的序号数为：" << rank << endl;
+
+			break;
+		}
+	case '2':
+		{
+			int rank;
+			deque<int> rank_deq;
+			cin >> rank;
+			while (rank)
 			{
-				cin >> element;
-				permutation.push_back(element);
-				cout << i;
+				rank_deq.push_front(rank % 10);
+				rank = rank / 10;
 			}
-			for (int i = 0; i < permutation.size(); i++)
+			switch (method)
 			{
-				cout << permutation.at(i);
+			case '1':
+				{
+
+				}
+			case '2':
+				{
+
+				}
+			case '3':
+				{
+
+				}
+			case '4':
+				{
+
+				}
+			default:
+				break;
 			}
 			break;
 		}
-		case '2':
+	case '3':
+		{
+			int intermediary;
+			deque<int> intermediary_deq;
+			cin >> intermediary;
+			while (intermediary)
+			{
+				intermediary_deq.push_front(intermediary % 10);
+				intermediary = intermediary / 10;
+			}
+			switch (method)
+			{
+			case '1':
+				{
 
-			cout << "zdnb";
+				}
+			case '2':
+				{
+
+				}
+			case '3':
+				{
+
+				}
+			case '4':
+				{
+
+				}
+			default:
+				break;
+			}
 			break;
+		}
+	default:
+		break;
 	}
 
 }
+
 
 int Dic_Permutation(int input)
 {
@@ -67,6 +242,7 @@ int Inc_Permutation(int input)
 
 int Dec_Permutation(int input)
 {
+
 	return 0;
 }
 
